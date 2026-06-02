06e4ffb65f5f3255d45879171cae5684b398b3dd
Muhammad Atif Qayyum <matifrao@gmail.com>
2026-06-02 23:02:33 +0500

    message

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-// =========================
-// BASIC JS FUNCTION
-// =========================
+console.log("script loaded");
 
-function handleClick() {
-  alert("Welcome! Website is ready to build further.");
+// EmailJS init
+emailjs.init("JLOehbAoWk5xJDFOx");
+
+// Elements
+const form = document.getElementById("contact-form");
+const status = document.getElementById("form-status");
+
+// Safety check
+if (!form) {
+  console.error("Form not found");
 }
+
+// Submit handler
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  status.textContent = "Sending...";
+  status.style.color = "#999";
+
+  emailjs.sendForm(
+    "service_4uhqudd",
+    "template_3b1k0jw",
+    form
+  )
+  .then((response) => {
+    console.log("SUCCESS:", response);
+
+    status.textContent = "Message sent successfully!";
+    status.style.color = "green";
+
+    form.reset();
+  })
+  .catch((error) => {
+    console.log("EMAILJS ERROR:", error);
+
+    status.textContent = "Failed to send message.";
+    status.style.color = "red";
+  });
+});
